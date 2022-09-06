401f5dc8a616625831dc962af4382eab142529c1
Nick Thorpe <nickthorpe71@gmail.com>
2022-09-05 21:46:00 -0500

    add chapter 1.3

--- cBookCourse/ch1.c
+++ cBookCourse/ch1.c
@@ -27,4 +27,14 @@ void printCtoF()
 	}
 }
 
+// 1.3 The For Statement
+
+void main() /* Fahrenheit-Celcius table */
+{
+  int fahr;
+
+	for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+	  printf("%4d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+}
+
 
