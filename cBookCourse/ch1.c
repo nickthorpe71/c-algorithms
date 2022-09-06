703f82369e738faec1bee4178a7bec98a9307ece
Nick Thorpe <nickthorpe71@gmail.com>
2022-09-05 21:55:45 -0500

    add seciton 1.4

--- cBookCourse/ch1.c
+++ cBookCourse/ch1.c
@@ -29,12 +29,33 @@ void printCtoF()
 
 // 1.3 The For Statement
 
-void main() /* Fahrenheit-Celcius table */
+void printCtoFFor() /* Fahrenheit-Celcius table */
 {
   int fahr;
 
 	for (fahr = 0; fahr <= 300; fahr = fahr + 20)
 	  printf("%4d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
+
+// 1.4 Symbolic Constnats
+
+# define LOWER 0   /* lower limit of table */
+# define UPPER 300 /* upper limit */
+# define STEP  20  /* step size */
+
+void printCtoFFor14() /* Fahrenheit-Celcius table */
+{
+  int fahr;
+
+	for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+	  printf("%4d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+}
+
+void main() 
+{
+  printCtoFFor14();
+}
+
+
 
 
