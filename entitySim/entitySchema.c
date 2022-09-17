c630f80e7b122b6612a0cdbd2a68b10af77fa479
Nick Thorpe <nickthorpe71@gmail.com>
2022-09-17 09:09:23 -0500

    add basic name gen

--- entitySim/entitySchema.c
+++ entitySim/entitySchema.c
@@ -1,17 +1,52 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct Entity {
     char name[10];
 };
 
+/**
+ * returns a random integer between min and max (inclusive)
+ */
+int randint(int min, int max) 
+{
+    if (min > max) return 0;
+    return rand() % (max - min) + min;
+}
+
+/**
+ * returns a random name with spedicfied length
+ * (max length 9)
+ */
+char* generateName(int length) 
+{
+    if (length > 9) length = 9;
+
+    char * name;
+    name = malloc(sizeof(char)*10);
+
+    // add random capital first letter
+    name[0] = (char)randint(65, 90);
+
+    // add random remaining letters
+    for (int i = 1; i < length; i++) 
+        name[i] = (char)randint(97, 122);
+    
+    name[length] = '\0';
+
+    return name;
+}
+
 void main() 
 {
-    srand(time(NULL));   // ONLY CALL ONCE
-    int r = rand() % 20; // number between 0 and 19
-    int a = rand() % 20; // number between 0 and 19
-    int n = rand() % 20; // number between 0 and 19
-    int d = rand() % 20; // number between 0 and 19
-    printf("%d %d %d %d\n", r, a, n, d);
+    // create random number seed
+    srand(time(NULL)); // ONLY CALL ONCE
+
+    for (int i = 0; i < 4; i++) {
+        char* newName = generateName(randint(2,10));
+        printf("%s\n", newName);
+        free(newName);
+    }
 }
