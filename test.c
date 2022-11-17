5557a6ddd50c5f639fab2857929fc07d834fec09
Nick Thorpe <nickthorpe71@gmail.com>
2022-11-16 21:05:12 -0600

    edit test program

--- test.c
+++ test.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
-int main()
+int main(int argc, char *argv[])
 {
-    printf("Hello, World!\n");
-    return 0;
+  float profit, inc, payout = 0;
+  int days;
+
+  sscanf(argv[1], "%f", &profit);
+  sscanf(argv[2], "%f", &inc);
+  sscanf(argv[3], "%i", &days);
+
+  for (int i = 0; i < days; i++)
+  {
+    profit *= inc;
+    if (profit > 500000)
+    {
+      profit -= 500;
+      payout += 500;
+    }
+  }
+
+  printf("total: %9.6f, payout:%9.6f\n", profit, payout);
+  return 0;
 }
