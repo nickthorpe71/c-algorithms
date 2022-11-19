fdab2223d2e6cec07420dde3c68011d2c9ab8f90
Nick Thorpe <nickthorpe71@gmail.com>
2022-11-19 09:35:34 -0600

    add end of chapter 3 question 3 solution

--- dataStructures/linkedList.c
+++ dataStructures/linkedList.c
@@ -80,6 +80,46 @@ void listMoveNextToFront(struct list *l, int item)
   }
 }
 
+void listSwap(struct list *l, int item1, int item2)
+{
+  struct node *p1 = l->head;
+  struct node *prev1 = NULL;
+  while (p1 != NULL)
+  {
+    if (p1->item == item1)
+      break;
+    prev1 = p1;
+    p1 = p1->next;
+  }
+
+  struct node *p2 = l->head;
+  struct node *prev2 = NULL;
+  while (p2 != NULL)
+  {
+    if (p2->item == item2)
+      break;
+    prev2 = p2;
+    p2 = p2->next;
+  }
+
+  if (p1 == NULL || p2 == NULL)
+    return;
+
+  if (prev1 != NULL)
+    prev1->next = p2;
+  else
+    l->head = p2;
+
+  if (prev2 != NULL)
+    prev2->next = p1;
+  else
+    l->head = p1;
+
+  struct node *tmp = p1->next;
+  p1->next = p2->next;
+  p2->next = tmp;
+}
+
 void listDelete(struct list *l, int item)
 {
   struct node *n = l->head;
@@ -128,6 +168,7 @@ int main(int argc, char *argv[])
   listAppend(&l, 43);
   listAppend(&l, 53);
   listInsertAfter(&l, 111, 3);
+  listSwap(&l, 2, 53);
   listDelete(&l, 1);
   listPrint(&l);
 
